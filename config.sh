f77dddeee226a993a63760994e4493604119c42c
Paweł Smolak <psmolak@iMac-Pawel.local>
2023-08-21 12:05:24 +0200

    Add known-hosts

--- config.sh
+++ config.sh
@@ -92,7 +92,7 @@ known-hosts-github() {
     echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk="
 }
 
-populate-ssh-known-hosts() {
+setup-known-hosts() {
     local hosts="$HOME/.ssh/known_hosts"
     
     if [ ! -f "$hosts" ]
@@ -121,7 +121,7 @@ setup-ssh() {
     add-ssh-host-config-entry "${keyname}" "${keypath}" "github.com"
     add-ssh-host-config-entry "${keyname}" "${keypath}" "bitbucket.org"
 
-    populate-ssh-known-hosts
+    setup-known-hosts
 }
 
 install-homebrew() {
@@ -228,21 +228,23 @@ post-setup() {
 
 main() {
     case "$1" in
-        "ssh")        setup-ssh ;;
-        "packages")   setup-packages ;;
-        "dotfiles")   setup-dotfiles ;;
-        "settings")   setup-settings ;;
-        "post-setup") post-setup ;;
+        "ssh")         setup-ssh ;;
+        "known-hosts") setup-known-hosts ;;
+        "packages")    setup-packages ;;
+        "dotfiles")    setup-dotfiles ;;
+        "settings")    setup-settings ;;
+        "post-setup")  post-setup ;;
 
         "all")
             setup-ssh
+            setup-known-hosts
             setup-packages
             setup-dotfiles
             setup-settings
             post-setup
             ;;
 
-        *) echo "Usage: ./setup.sh ssh | packages | dotfiles | settings | post-setup" ;;
+        *) echo "Usage: ./setup.sh ssh | known-hosts | packages | dotfiles | settings | post-setup" ;;
     esac
 }
 
